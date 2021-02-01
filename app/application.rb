cfb857341ff5929e57b409ca8e3c553ac1e33ef0
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-01 10:14:37 -0800

    commit tree 9e35372091e67c68f5f7dc30b97def7f2230d828

---
+++ app/application.rb
@@ -0,0 +1,33 @@
+# Your application should only accept the /items/<ITEM NAME> route. 
+# Everything else should 404
+
+# If a user requests /items/<Item Name> it should return the price of that item
+
+# IF a user requests an item that you don't have, then return a 400 and an 
+# error message
+
+class Application
+
+    @@items = []
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+           item_name = req.path.split("/items/").last
+           item = @@items.find{|i| i.name == item_name}
+           if @@items.include?(item)
+            resp.write item.price
+            resp.status = 200
+           else
+           resp.write "Item not found"
+           resp.status = 400
+           end
+        else
+            resp.status = 404   
+            resp.write "Route not found"
+        end
+        resp.finish
+    end
+end
